b5cd1d108ed96090cf89f9e9d428faae21c5840e
SeeuSim <seeusimong@gmail.com>
2023-03-14 13:02:42 +0800

    Add Base db connectivity, update requirements
    - Once functions are migrated from typescript, azure migration will be
      ready.

--- app/db/database.py
+++ app/db/database.py
@@ -1,13 +1,29 @@
 from prisma import Prisma
 from prisma.models import *
+from prisma.types import CollectionCreateInput
+from decimal import Decimal
 
-db = Prisma(auto_register=True)
+db = Prisma(auto_register=True, use_dotenv=True)
 
-async def main():
+async def p_main():
   await db.connect()
-  collection = await Collection.prisma().create(
-    data={
+
+  data_point: CollectionCreateInput = {
+      'address': '0x432',
+      'tokens': 0,
+      'owners': 0,
+      'salesVolume': Decimal('0'),
+      'image': "",
+      'bannerImg': ""
+    }
+
+  collection = await Collection.prisma().upsert(
+    where={
       'address': '0x432'
+    },
+    data={
+      'create': data_point,
+      'update': data_point
     }
   )
   await db.disconnect()
--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from db.database import *
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
+  await p_main()
   return { "message": "Hello World"}
