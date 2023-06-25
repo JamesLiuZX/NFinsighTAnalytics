c7db0721bd3b8d3bb5713497329c081cf300392e
SeeuSim <seeusimong@gmail.com>
2023-06-25 18:04:54 +0800

    Update server rehresh script

--- scripts/refresh_job.sh
+++ scripts/refresh_job.sh
@@ -1,3 +1,14 @@
+cd /path/to/repo
+
+#Start servers in detached mode
+echo "Starting Servers"
+docker compose up -d
+
+echo "Servers started :)"
+sleep 45 # Give servers time to start
+
+# Run refresh job
+echo "Starting job"
 export USER_NAME="{USERNAME}"
 export PASSWORD="{PASSWORD}"
 export HOSTNAME="{HOSTNAME}"
@@ -12,8 +23,16 @@ curl -X 'GET' "http://${HOSTNAME}/nft/refresh" \
   -H 'accept: application/json' \
   -H "Authorization: Bearer ${TOKEN}"
 
+echo "Job finished"
+sleep 120 # If job runs longer, and response times out
+
 # curl -X 'GET' \
   # "http://${HOSTNAME}/upsert_collection_data?contract_address=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D&duration=DURATION_1_DAY" \
   # -H 'accept: application/json' \
   # -H "Authorization: Bearer ${TOKEN}"
+
+echo "Deleting servers"
+docker compose down
+
+echo "Servers deleted :) Have a nice day :p"
 
